da31c6a2053db5d21471709fe75530606304b874
d0kur0 <dev.dokuro@gmail.com>
2021-08-05 00:42:05 +0300

    Я не помню что я тут делал, но файлы не пушнуты

---
+++ src/views/OpenAPI.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import "@d0kur0/open-api/dist/open-api.css"
+import OpenAPI from "@d0kur0/open-api";
+
+export default defineComponent({
+  name: "Home",
+  components: {
+    OpenAPI,
+  },
+}); 
+</script>
+  
+<template>
+  <OpenAPI />
+</template> 
+
