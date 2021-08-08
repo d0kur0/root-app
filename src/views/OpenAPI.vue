0ac0d5f07a3a1d195f893e16060f5ebfb606afd8
d0kur0 <dev.dokuro@gmail.com>
2021-08-09 02:46:16 +0300

    Commit all changes

--- src/views/OpenAPI.vue
+++ src/views/OpenAPI.vue
@@ -1,17 +1,24 @@
 <script lang="ts">
 import { defineComponent } from "vue";
-import "@d0kur0/open-api/dist/open-api.css"
+
+import "@d0kur0/open-api/dist/open-api.css";
+import "@d0kur0/uikit/dist/uikit.css";
+
 import OpenAPI from "@d0kur0/open-api";
+import { Button } from "@d0kur0/uikit";
 
 export default defineComponent({
   name: "Home",
   components: {
     OpenAPI,
+    Button
   },
 }); 
 </script>
   
 <template>
   <OpenAPI />
+  <Button>Hoba</Button>
+  <Button :transparent="true">Hoba</Button>
 </template> 
 
